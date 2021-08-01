26c17f1109c34609908504235177b16e2acdb021
Ricardo Massaro <ricardo.massaro@gmail.com>
2021-08-01 19:41:12 -0300

    Clean up MOD player

--- lib/mod_play.c
+++ lib/mod_play.c
@@ -28,6 +28,7 @@
 #endif
 
 #define MOD_REF_CLOCK     7093789  // 7159090 for NTSC
+#define MOD_MAX_CHANNELS  4
 
 // channel
 struct PLAY_CHANNEL {
@@ -42,51 +43,47 @@ struct PLAY_CHANNEL {
 };
 
 struct PLAY_STATE {
-  struct PLAY_CHANNEL channels[4];
+  const struct MOD_DATA *mod;
+  struct PLAY_CHANNEL channels[MOD_MAX_CHANNELS];
+  
+  uint32_t out_frequency;           // output frequency (samples per second)
+  uint32_t sample_advance_factor;   // used to calculate sample playing frequency
 
-  unsigned char should_loop_mod;
-  unsigned char mod_ended;
+  uint8_t loop_enabled;             // 1 if mod should be looped
+  uint8_t jump_enabled;             // 1 to jump before starting the next row
+  uint8_t jump_to_song_pos;         // jump song_pos destination (if jump_enabled)
+  uint8_t jump_to_row;              // jump row destination (if jump enabled)
   
-  unsigned char jump_enabled;
-  unsigned char jump_to_song_pos;
-  unsigned char jump_to_row;
+  int32_t ticks_per_row;            // number of ticks per row (defined by mod speed)
+  int32_t samples_per_tick;         // number of output samples per tick (defined by mod speed)
   
-  int ticks_per_row;
-  int samples_per_tick;
-  int beats_per_minute;
-  
-  int cur_song_pos;
-  int cur_pattern;
-  int cur_row;
-
-  int cur_tick;
-  int cur_tick_sample;
+  int32_t cur_song_pos;             // current song pos (index into mod->song_positions)
+  int32_t cur_pattern;              // current pattern number (as given by mod->song_positions[cur_song_pos])
+  int32_t cur_row;                  // current row in current pattern
+  int32_t cur_tick;                 // current tick in current row
+  int32_t cur_tick_sample;          // current output sample in current tick
 };
 
-static uint32_t play_frequency;
-static uint32_t sample_advance_factor;
 static struct PLAY_STATE state;
-static const struct MOD_DATA *mod;
 
-static int start_row(void);
-
-// returns fixed point 20.12
+// return the number of input samples to advance per output sample for a given note period (fixed point 20.12)
 static uint32_t get_sample_advance_for_period(unsigned int period)
 {
-  return sample_advance_factor / period;
+  return state.sample_advance_factor / period;
 }
 
+// return the number of output samples per tick for the given bpm
 static uint32_t get_samples_per_tick(unsigned int bpm)
 {
-  return play_frequency * 5 / bpm / 2;
+  return state.out_frequency * 5 / bpm / 2;
 }
 
 static void reset_channels(void)
 {
-  for (int c = 0; c < mod->num_channels; c++) {
+  for (int c = 0; c < state.mod->num_channels; c++) {
     struct PLAY_CHANNEL *ch = &state.channels[c];
     ch->playing        = 0;
-    ch->sample         = &mod->samples[0];
+    ch->sample         = &state.mod->samples[0];
     ch->sample_pos     = 0;
     ch->sample_len     = 0;
     ch->volume         = 64;
@@ -96,31 +93,6 @@ static void reset_channels(void)
   }
 }
 
-void mod_play_start(const struct MOD_DATA *mod_data, unsigned int out_frequency, int loop)
-{
-  mod = mod_data;
-  play_frequency = out_frequency;
-  sample_advance_factor = (((((uint32_t)MOD_REF_CLOCK)/2)<<10) / out_frequency)<<2;
-
-  reset_channels();
-  
-  state.should_loop_mod = loop;
-  state.mod_ended = 0;
-  state.jump_enabled = 0;
-  
-  state.samples_per_tick = get_samples_per_tick(125);
-  state.ticks_per_row = 6;
-
-  state.cur_tick = 0;
-  state.cur_tick_sample = 0;
-  
-  state.cur_song_pos = 0;
-  state.cur_pattern = mod->song_positions[state.cur_song_pos];
-  state.cur_row = 0;
-
-  start_row();
-}
- 
 static void process_channel_effect(int chan_num)
 {
   struct PLAY_CHANNEL *ch = &state.channels[chan_num];
@@ -148,7 +120,7 @@ static void process_channel_effect(int chan_num)
 
   case 0x5: break; // slide to note into volume slide (processed in tick)
 
-  case 0x6: break; // vibrato into volume slide (processed in ticl)
+  case 0x6: break; // vibrato into volume slide (processed in tick)
 
   case 0x7: // tremolo [x][y] adjust volume: (x=frequency, y=amplitude)
     ALERT("                                                    ch[%d]: TREMOLO %02x !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n", chan_num, ch->effect & 0xff);
@@ -372,34 +344,37 @@ static int start_row(void)
 {
   if (state.jump_enabled) {    // process jump to new song position
     // only jump back if looping is enabled
-    if (state.jump_to_song_pos > state.cur_song_pos || state.should_loop_mod) {
+    if (state.jump_to_song_pos > state.cur_song_pos || state.loop_enabled) {
       state.cur_song_pos = state.jump_to_song_pos;
       state.cur_row = state.jump_to_row;
-      state.cur_pattern = mod->song_positions[state.cur_song_pos];
+      state.cur_pattern = state.mod->song_positions[state.cur_song_pos];
       state.jump_enabled = 0;
     }
   }
   
   if (state.cur_row >= 64) {   // scroll to next song position
     state.cur_row = 0;
-    if (++state.cur_song_pos >= mod->num_song_positions) {
-      if (! state.should_loop_mod) {
-        state.mod_ended = 1;
+    if (++state.cur_song_pos >= state.mod->num_song_positions) {
+      if (! state.loop_enabled) {
+        state.mod = NULL;
         return 1;
       }
+      // restart from beginning
+      state.samples_per_tick = get_samples_per_tick(125);
+      state.ticks_per_row = 6;
       state.cur_song_pos = 0;
       reset_channels();
     }
-    state.cur_pattern = mod->song_positions[state.cur_song_pos];
+    state.cur_pattern = state.mod->song_positions[state.cur_song_pos];
     DEBUG("--- pattern ------------\n");
   }
   
   DEBUG("patt %2d row %2d\n", state.cur_pattern, state.cur_row);
-  const struct MOD_CELL *cell = &mod->pattern[mod->num_channels * (64*state.cur_pattern + state.cur_row)];
-  for (int c = 0; c < mod->num_channels; c++) {
+  const struct MOD_CELL *cell = &state.mod->pattern[state.mod->num_channels * (64*state.cur_pattern + state.cur_row)];
+  for (int c = 0; c < state.mod->num_channels; c++) {
     struct PLAY_CHANNEL *ch = &state.channels[c];
     if (cell->sample > 0) {
-      ch->sample = &mod->samples[cell->sample-1];
+      ch->sample = &state.mod->samples[cell->sample-1];
       ch->sample_pos = 0;
       ch->sample_len = (ch->sample->loop_len > 2) ? (ch->sample->loop_start+ch->sample->loop_len) : ch->sample->len;
       ch->playing = 1;
@@ -431,7 +406,7 @@ static int start_tick(void)
   }
 
   if (state.cur_tick > 0) {
-    for (int c = 0; c < mod->num_channels; c++) {
+    for (int c = 0; c < state.mod->num_channels; c++) {
       process_tick_channel_effect(c);
     }
   }
@@ -450,7 +425,7 @@ static void play_samples(unsigned char *out, unsigned int n_samples)
 {
   for (unsigned int i = 0; i < n_samples; i++) {
     int out_sample = 0;
-    for (int c = 0; c < mod->num_channels; c++) {
+    for (int c = 0; c < state.mod->num_channels; c++) {
       struct PLAY_CHANNEL *ch = &state.channels[c];
 
       if (ch->playing && ch->sample->data && (ch->sample_pos>>12) < ch->sample_len) {
@@ -471,7 +446,7 @@ static void play_samples(unsigned char *out, unsigned int n_samples)
 
 int mod_play_step(unsigned char *out, unsigned int len)
 {
-  if (state.mod_ended) {
+  if (! state.mod) {
     memset(out, 128, len);
     return 1;
   }
@@ -496,4 +471,29 @@ int mod_play_step(unsigned char *out, unsigned int len)
     out += num_samples;
   }
   return 0;
+}
+
+void mod_play_start(const struct MOD_DATA *mod_data, unsigned int out_frequency, int loop)
+{
+  if (mod_data->num_channels > MOD_MAX_CHANNELS) return;
+
+  state.mod = mod_data;
+  state.out_frequency = out_frequency;
+  state.sample_advance_factor = (((((uint32_t)MOD_REF_CLOCK)/2)<<10) / out_frequency)<<2;   // MOD_REF_CLOCK/(2*out_frequency) in 20.12 fixed point
+
+  state.samples_per_tick = get_samples_per_tick(125);
+  state.ticks_per_row = 6;
+
+  state.cur_song_pos = 0;
+  state.cur_pattern = state.mod->song_positions[state.cur_song_pos];
+  state.cur_row = 0;
+
+  state.cur_tick = 0;
+  state.cur_tick_sample = 0;
+  
+  state.loop_enabled = loop;
+  state.jump_enabled = 0;
+  
+  reset_channels();
+  start_row();
 }
